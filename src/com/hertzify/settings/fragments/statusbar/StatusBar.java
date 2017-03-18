8a89fceba1af3fd6e63707feb5978cebc6182e67
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    Hertzify: Add ability to toggle bluetooth battery level [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaba0b0781d890d7ad7c4b78ade9e10220bb04524

--- src/com/hertzify/settings/fragments/statusbar/StatusBar.java
+++ src/com/hertzify/settings/fragments/statusbar/StatusBar.java
@@ -21,6 +21,9 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.hertzify.settings.utils.DeviceUtils;
+import com.hertzify.settings.preferences.SystemSettingSwitchPreference;
+
 import java.util.List;
 
 @SearchIndexable
@@ -28,6 +31,12 @@ public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "StatusBar";
+
+    private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
+    private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
+
+    private PreferenceCategory mIconsCategory;
+    private SystemSettingSwitchPreference mBluetoothBatteryStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,13 @@ public class StatusBar extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
+        mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
+
+        if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+            mIconsCategory.removePreference(mBluetoothBatteryStatus);
+        }
     }
 
     @Override
@@ -59,6 +75,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
+                if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+                    keys.add(KEY_BLUETOOTH_BATTERY_STATUS);
+                }
                 return keys;
             }
         };
